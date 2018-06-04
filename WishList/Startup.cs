7bdb05b24ba7bc2ef6f821eb908d996023e9bbab
Eric J Fisher <me@ericjfisher.com>
2018-06-04 15:10:08 -0400

    Revert "First Task"
    This reverts commit 58cebc72fd59b41b1234bf02ede44a6296e7873b.

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -16,13 +16,15 @@ namespace WishList
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseMvcWithDefaultRoute();
-        }
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        } BREAK STUFF!!!!
     }
 }
