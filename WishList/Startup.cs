63783f44c0802357885c56940e50da8e3ea9262d
Eric J Fisher <me@ericjfisher.com>
2018-06-04 14:57:26 -0400

    Revert "Task 2!!! If it's good WE GO LIVE!!!"
    This reverts commit d30745b32f4f5707cdf90593782ca9d804447394.

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -22,15 +22,6 @@ namespace WishList
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if(env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-
             app.UseMvcWithDefaultRoute();
         }
     }
