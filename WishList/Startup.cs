5fa413e7f4afee79ef674ec2a6cdd84bd6775103
Eric J Fisher <eric-fisher@pluralsight.com>
2019-04-17 11:13:14 -0400

    Task 2 Configure MVC Middleware in Configure Complete

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -22,10 +22,7 @@ namespace WishList
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
