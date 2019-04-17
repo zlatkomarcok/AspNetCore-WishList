8318328da297ab77d6bf6a36203b71d0841f0953
Eric J Fisher <eric-fisher@pluralsight.com>
2019-04-17 13:51:32 -0400

    Task 3 Configure EntityFramework Complete

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using WishList.Data;
 
 namespace WishList
 {
@@ -17,6 +18,7 @@ namespace WishList
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("Wishlist"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
