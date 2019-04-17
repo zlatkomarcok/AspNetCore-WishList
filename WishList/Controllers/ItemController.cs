cd042129728551a9612a9449fa50d5febe1f227e
Eric J Fisher <eric-fisher@pluralsight.com>
2019-04-17 14:19:39 -0400

    Task 1 Create ItemController Complete

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
