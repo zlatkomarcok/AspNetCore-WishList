fe9d4ea944d9828bbb5ea313e448d8aa571a111d
Eric J Fisher <eric-fisher@pluralsight.com>
2019-04-17 13:49:40 -0400

    Task 2 Add Constructor to ApplicationDbContext Complete

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace WishList.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
     }
 }
