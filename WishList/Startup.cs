e88a3a2756ae76789d23dcdd2fe16f84a95ab769
Eric J Fisher <eric-fisher@pluralsight.com>
2020-01-09 18:24:43 -0500

    Updated code to align with .NET 3.1

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -19,7 +19,7 @@ namespace WishList
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.Run(async (context) =>
             {
