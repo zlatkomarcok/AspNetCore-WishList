001ef82459262356bb0e47662ebf953b3ed839b2
Eric J Fisher <eric-fisher@pluralsight.com>
2020-01-10 12:38:37 -0500

    Updated instructions and tests to align with 3.1 changes

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace WishList
 {
