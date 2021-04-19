20f5c73882f4c70d7bfca00cf3cd3aaa4bf6c447
zlatkomarcok <82848779+zlatkomarcok@users.noreply.github.com>
2021-04-19 19:25:12 -0400

    Update Startup.cs

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WishList.Data;
 
 namespace WishList
 {
@@ -17,6 +18,7 @@ namespace WishList
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("WishList"));
             services.AddMvc();
         }
 
