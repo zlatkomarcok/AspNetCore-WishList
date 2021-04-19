27046784d8ea8c6aa83d9fcfaba2bb366c3e9d43
zlatkomarcok <82848779+zlatkomarcok@users.noreply.github.com>
2021-04-19 19:21:51 -0400

    asdf

---
+++ WishList/Models/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WishList.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Description { get; set; }
+
+
+    }
+}
